a08b16a0737374f1290dc8e0eddef65710b5e8ca
Dzaky Haidar <haidarmuhammaddzaky@gmail.com>
2026-04-16 14:38:45 +0700

    nganu api endpoint

--- app/presentation/api/router.py
+++ app/presentation/api/router.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 
 from app.presentation.api.routes import health, ranking, root
 
-api_router = APIRouter()
+api_router = APIRouter(prefix="/api")
 api_router.include_router(root.router)
 api_router.include_router(health.router)
 api_router.include_router(ranking.router)
